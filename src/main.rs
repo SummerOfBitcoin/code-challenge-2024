4861ba501133bdb4b859a254201456001e0e5768
Gabriel Gustavo Nicolodi <gabriel.gnicolodi@gmail.com>
2024-04-15 00:31:24 -0300

    Testing the implementation of PoW using ring's sha256 implementation

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ mod block;
 use block::block_header::BlockHeader;
 use chrono::Utc;
 use mining_challenge::read_mempool;
-// use sha2::{Sha256, Digest};
+use ring::digest::{Context, Digest, SHA256};
+use std::fmt::Write;
 // use hex_literal::hex;
 
 fn main() {
@@ -12,6 +13,35 @@ fn main() {
     let first_block_header: BlockHeader = BlockHeader::new(String::from("00000000000000000000000000000000"), String::from("00000000000000000000000000000000"), Utc::now(), 128);
 
     println!("{}", first_block_header);
+
+    let data: &str = "texto 1234";
+
+    let mut nonce: u64 = 0_u64;
+
+    loop{
+
+        let mut context: Context = Context::new(&SHA256);
+        
+        let target_hash: String = String::from("0000ffff00000000000000000000000000000000000000000000000000000000");
+
+        context.update(data.as_bytes());
+        context.update(&nonce.to_be_bytes());
+        
+        let digest: Digest = context.finish();
+        let mut actual_hex: String = String::new();
+        
+        for &byte in digest.as_ref() {
+            write!(&mut actual_hex, "{:02x}", byte).expect("Failed to write hex");
+        }
+        
+        println!("Nonce: {}, Hash: {}", nonce, actual_hex);
+
+        if actual_hex <= target_hash {
+            println!("Found the nonce for the target Hash! It is: {} and you can attach this block to the blockchain", nonce);
+            break
+        }
+        nonce += 1;
+    }
 }
 
 
