33c5d59a058dd013b2be157a679526c844eed4be
Zaid <predcodes695@gmail.com>
2024-04-17 21:07:10 +0530

    test: reverted

--- witnessMerkle.go
+++ witnessMerkle.go
@@ -3,25 +3,23 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
-	"os"
-	"strings"
 )
 
 func CreateWitnessMerkle() string {
 	_, _, wTxIDs := Prioritize()
 	wTxIDs = append([]string{"0000000000000000000000000000000000000000000000000000000000000000"}, wTxIDs...)
-	var wtxidsnew []string
-	file, _ := os.ReadFile("wtxids.txt")
-	for _, line := range strings.Split(string(file), "\n") {
-		wtxidsnew = append(wtxidsnew, line)
-	}
-	for idx := range wtxidsnew {
-		byt, _ := hex.DecodeString(wtxidsnew[idx])
-		byt = reverseBytes(byt)
-		wtxidsnew[idx] = hex.EncodeToString(byt)
-	}
-	fmt.Println(len(wtxidsnew))
-	merkleRoot := NewMerkleTree(wtxidsnew)
+	// var wtxidsnew []string
+	// file, _ := os.ReadFile("wtxids.txt")
+	// for _, line := range strings.Split(string(file), "\n") {
+	// 	wtxidsnew = append(wtxidsnew, line)
+	// }
+	// for idx := range wtxidsnew {
+	// 	byt, _ := hex.DecodeString(wtxidsnew[idx])
+	// 	byt = reverseBytes(byt)
+	// 	wtxidsnew[idx] = hex.EncodeToString(byt)
+	// }
+	// fmt.Println(len(wtxidsnew))
+	merkleRoot := NewMerkleTree(wTxIDs)
 	fmt.Println("WMKR: ", hex.EncodeToString(merkleRoot.Data))
 	commitment_string := hex.EncodeToString(merkleRoot.Data) + "0000000000000000000000000000000000000000000000000000000000000000"
 	WitnessCommitment, _ := hex.DecodeString(commitment_string)
