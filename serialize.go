116c98f562ed0091619f3625b2277e76f305851b
Zaid <predcodes695@gmail.com>
2024-04-19 01:51:44 +0530

    add: ah

--- serialize.go
+++ serialize.go
@@ -8,31 +8,31 @@ import (
 	"os"
 )
 
-type Input struct {
-	TxID         string   `json:"txid"`
-	Vout         uint32   `json:"vout"`
-	Prevout      Prevout  `json:"prevout"`
-	Scriptsig    string   `json:"scriptsig"`
-	ScriptsigAsm string   `json:"scriptsig_asm"`
-	Witness      []string `json:"witness"`
-	IsCoinbase   bool     `json:"is_coinbase"`
-	Sequence     uint32   `json:"sequence"`
-}
+	type Input struct {
+		TxID         string   `json:"txid"`
+		Vout         uint32   `json:"vout"`
+		Prevout      Prevout  `json:"prevout"`
+		Scriptsig    string   `json:"scriptsig"`
+		ScriptsigAsm string   `json:"scriptsig_asm"`
+		Witness      []string `json:"witness"`
+		IsCoinbase   bool     `json:"is_coinbase"`
+		Sequence     uint32   `json:"sequence"`
+	}
 
-type Prevout struct {
-	Scriptpubkey        string `json:"scriptpubkey"`
-	ScriptpubkeyAsm     string `json:"scriptpubkey_asm"`
-	ScriptpubkeyType    string `json:"scriptpubkey_type"`
-	ScriptpubkeyAddress string `json:"scriptpubkey_address"`
-	Value               uint64 `json:"value"`
-}
+	type Prevout struct {
+		Scriptpubkey        string `json:"scriptpubkey"`
+		ScriptpubkeyAsm     string `json:"scriptpubkey_asm"`
+		ScriptpubkeyType    string `json:"scriptpubkey_type"`
+		ScriptpubkeyAddress string `json:"scriptpubkey_address"`
+		Value               uint64 `json:"value"`
+	}
 
-type Transaction struct {
-	Version  uint32    `json:"version"`
-	Locktime uint32    `json:"locktime"`
-	Vin      []Input   `json:"vin"`
-	Vout     []Prevout `json:"vout"`
-}
+	type Transaction struct {
+		Version  uint32    `json:"version"`
+		Locktime uint32    `json:"locktime"`
+		Vin      []Input   `json:"vin"`
+		Vout     []Prevout `json:"vout"`
+	}
 
 var strs []byte
 
--- witnessMerkle.go
+++ witnessMerkle.go
@@ -8,17 +8,6 @@ import (
 func CreateWitnessMerkle() string {
 	_, _, wTxIDs := Prioritize()
 	wTxIDs = append([]string{"0000000000000000000000000000000000000000000000000000000000000000"}, wTxIDs...)
-	// var wtxidsnew []string
-	// file, _ := os.ReadFile("wtxids.txt")
-	// for _, line := range strings.Split(string(file), "\n") {
-	// 	wtxidsnew = append(wtxidsnew, line)
-	// }
-	// for idx := range wtxidsnew {
-	// 	byt, _ := hex.DecodeString(wtxidsnew[idx])
-	// 	byt = reverseBytes(byt)
-	// 	wtxidsnew[idx] = hex.EncodeToString(byt)
-	// }
-	// fmt.Println(len(wtxidsnew))
 	merkleRoot := NewMerkleTree(wTxIDs)
 	fmt.Println("WMKR: ", hex.EncodeToString(merkleRoot.Data))
 	commitment_string := hex.EncodeToString(merkleRoot.Data) + "0000000000000000000000000000000000000000000000000000000000000000"
