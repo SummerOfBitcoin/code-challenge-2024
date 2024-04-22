939c3244d8894650b923b6c578bd61fc428616a0
ajaysehwal <ajaysehwal000@gmail.com>
2024-04-22 14:04:28 +0530

    fix : change difficulty

--- dist/index.js
+++ dist/index.js
@@ -56,13 +56,14 @@ class MineBlock {
     start() {
         return __awaiter(this, void 0, void 0, function* () {
             const target = BigInt("0x" + this.difficulty);
-            const header = Buffer.from(this.block.headerBuffer());
+            const header = this.block.headerBuffer();
             this.block.hash = (0, utils_1.doubleSHA256)(header).toString('hex');
             while (BigInt(`0x${this.block.hash}`) >= target) {
                 this.block.nonce += 1;
                 header.writeUInt32LE(this.block.nonce, 80 - 4);
                 this.block.hash = (0, utils_1.doubleSHA256)(header).toString('hex');
                 this.hashes++;
+                console.log(this.block.nonce, this.block.hash);
                 if (this.hashes % 1000000 === 0) {
                     console.log(`Iteration ${this.hashes}: ${this.block.hash}`);
                 }
