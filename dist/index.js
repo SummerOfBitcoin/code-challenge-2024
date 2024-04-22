f3f6971d8a3edd01f8fdd3a4bff65f91d73d084c
ajaysehwal <ajaysehwal000@gmail.com>
2024-04-22 21:46:33 +0530

    fix : bugs

--- dist/index.js
+++ dist/index.js
@@ -58,12 +58,12 @@ class MineBlock {
             const target = BigInt("0x" + this.difficulty);
             const header = this.block.headerBuffer();
             this.block.hash = (0, utils_1.doubleSHA256)(header).toString('hex');
-            while (BigInt(`0x${this.block.hash}`) >= target) {
+            while (BigInt(`0x${this.block.hash}`) > target) {
                 this.block.nonce += 1;
                 header.writeUInt32LE(this.block.nonce, 80 - 4);
                 this.block.hash = (0, utils_1.doubleSHA256)(header).toString('hex');
                 this.hashes++;
-                // console.log(this.block.nonce,this.block.hash);
+                console.log(this.block.nonce, this.block.hash);
                 if (this.hashes % 1000000 === 0) {
                     console.log(`Iteration ${this.hashes}: ${this.block.hash}`);
                 }
