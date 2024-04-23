1a9401375c618f94652cda5f74968c69deaaa224
ajaysehwal <ajaysehwal000@gmail.com>
2024-04-23 19:30:53 +0530

    fix : bug

--- dist/validate.js
+++ dist/validate.js
@@ -33,7 +33,7 @@ class Validator {
             }
         }
         console.log(`Find ${this.ValidTXCount} are valid and ${this.InValidTXCount} invalid`);
-        for (let i = 0; i < 200; i++) {
+        for (let i = 0; i < 400; i++) {
             validTransactions.pop();
         }
         return validTransactions;
