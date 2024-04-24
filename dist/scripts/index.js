b59d9de8dfb2b6ba7a3fb6729abfd9a45d53d4f8
ajaysehwal <ajaysehwal000@gmail.com>
2024-04-24 11:21:40 +0530

    fix : bug

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BitcoinScript = void 0;
-const p2pkh_1 = require("./p2pkh");
+const p2wpkh_1 = require("./p2wpkh");
 class BitcoinScript {
     constructor(transaction) {
         this.transaction = transaction;
@@ -13,20 +13,20 @@ class BitcoinScript {
             let scriptPubKey, scriptSig, witness, publicKeyHash;
             let isValidInput = false;
             switch (scripttype) {
-                case "p2pkh":
+                // case "p2pkh":
+                //   scriptPubKey = input.prevout.scriptpubkey_asm;
+                //   scriptSig = input.scriptsig_asm;
+                //   const p2pkh = new P2PKH(this.transaction);
+                //   isValidInput = p2pkh.execute(scriptPubKey, scriptSig, i);
+                //   break;
+                case "v0_p2wpkh":
                     scriptPubKey = input.prevout.scriptpubkey_asm;
-                    scriptSig = input.scriptsig_asm;
-                    const p2pkh = new p2pkh_1.P2PKH(this.transaction);
-                    isValidInput = p2pkh.execute(scriptPubKey, scriptSig, i);
+                    publicKeyHash = scriptPubKey.split(" ")[2];
+                    scriptPubKey = `OP_DUP OP_HASH160 ${publicKeyHash} OP_EQUALVERIFY OP_CHECKSIG`;
+                    witness = input.witness;
+                    const p2wpkh = new p2wpkh_1.P2WPKH(this.transaction);
+                    isValidInput = p2wpkh.execute(scriptPubKey, witness, i);
                     break;
-                // case "v0_p2wpkh":
-                // scriptPubKey = input.prevout.scriptpubkey_asm;
-                // publicKeyHash = scriptPubKey.split(" ")[2];
-                // scriptPubKey = `OP_DUP OP_HASH160 ${publicKeyHash} OP_EQUALVERIFY OP_CHECKSIG`;
-                // witness = input.witness;
-                // const p2wpkh = new P2WPKH(this.transaction);
-                // isValidInput = p2wpkh.execute(scriptPubKey, witness, i);
-                // break;
                 default:
                     break;
             }
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -12,20 +12,20 @@ export class BitcoinScript {
       let isValidInput = false;
 
       switch (scripttype) {
-        case "p2pkh":
+        // case "p2pkh":
+        //   scriptPubKey = input.prevout.scriptpubkey_asm;
+        //   scriptSig = input.scriptsig_asm;
+        //   const p2pkh = new P2PKH(this.transaction);
+        //   isValidInput = p2pkh.execute(scriptPubKey, scriptSig, i);
+        //   break;
+          case "v0_p2wpkh":
           scriptPubKey = input.prevout.scriptpubkey_asm;
-          scriptSig = input.scriptsig_asm;
-          const p2pkh = new P2PKH(this.transaction);
-          isValidInput = p2pkh.execute(scriptPubKey, scriptSig, i);
+          publicKeyHash = scriptPubKey.split(" ")[2];
+          scriptPubKey = `OP_DUP OP_HASH160 ${publicKeyHash} OP_EQUALVERIFY OP_CHECKSIG`;
+          witness = input.witness;
+          const p2wpkh = new P2WPKH(this.transaction);
+          isValidInput = p2wpkh.execute(scriptPubKey, witness, i);
           break;
-          // case "v0_p2wpkh":
-          // scriptPubKey = input.prevout.scriptpubkey_asm;
-          // publicKeyHash = scriptPubKey.split(" ")[2];
-          // scriptPubKey = `OP_DUP OP_HASH160 ${publicKeyHash} OP_EQUALVERIFY OP_CHECKSIG`;
-          // witness = input.witness;
-          // const p2wpkh = new P2WPKH(this.transaction);
-          // isValidInput = p2wpkh.execute(scriptPubKey, witness, i);
-          // break;
         default:
           break;
       }
