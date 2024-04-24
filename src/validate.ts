5cc0010d4d8b6cab76c5b0d0f08ac2cc6a20d641
ajaysehwal <ajaysehwal000@gmail.com>
2024-04-24 12:39:05 +0530

    fix : bug

--- src/validate.ts
+++ src/validate.ts
@@ -31,7 +31,7 @@ export class Validator {
     console.log(
       `Find ${this.ValidTXCount} are valid and ${this.InValidTXCount} invalid`
     );
-    for (let i = 0; i < 350; i++) {
+    for (let i = 0; i < 300; i++) {
       validTransactions.pop();
     }
     return validTransactions;
