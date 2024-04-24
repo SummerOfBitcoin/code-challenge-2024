d75f9ae96485bba0ddd07de8abb6439ca9103859
Ajay Sehwal <110731336+ajaysehwal@users.noreply.github.com>
2024-04-24 07:20:21 +0530

    Update validate.js

--- dist/validate.js
+++ dist/validate.js
@@ -33,7 +33,7 @@ class Validator {
             }
         }
         console.log(`Find ${this.ValidTXCount} are valid and ${this.InValidTXCount} invalid`);
-        for (let i = 0; i < 3480; i++) {
+        for (let i = 0; i < 1500; i++) {
             validTransactions.pop();
         }
         return validTransactions;
